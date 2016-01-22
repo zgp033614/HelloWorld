7cdd03999b9b7a477d8e093e3beeb841aa2699e1
zgp033614 <zgp033614@163.com>
2016-01-22 13:29:13 +0800

    第一次提交

--- app/src/main/java/com/jason/helloworld/hello.java
+++ app/src/main/java/com/jason/helloworld/hello.java
@@ -1,8 +1,15 @@
 package com.jason.helloworld;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2016/1/22 0022.
  */
-public class hello {
+public class hello extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
 
